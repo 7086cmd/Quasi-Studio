b5ae054f21705970649aa41201d100c6fcb59918
Wu Chengyu <7086cmd@gmail.com>
2022-02-06 22:58:50 +0800

    Start this project.

---
+++ packages/pages/vite.config.ts
@@ -0,0 +1,82 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import lagacy from '@vitejs/plugin-legacy'
+import { VitePWA as pwa } from 'vite-plugin-pwa'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    lagacy({
+      targets: ['defaults'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+    pwa({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Magnifique',
+        short_name: 'Magn.',
+        description: 'A great management platform.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+  build: {
+    outDir: './dist/pages',
+  },
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove()
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
+})
